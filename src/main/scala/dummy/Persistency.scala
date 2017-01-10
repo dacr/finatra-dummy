e6b86c04f7f8890fb250dcb3ea6b961226b07e12
Crosson David <crosson.david@gmail.com>
2017-01-10 22:21:36 +0100

    no more await at all curl http://localhost:8888/message

--- src/main/scala/dummy/Persistency.scala
+++ src/main/scala/dummy/Persistency.scala
@@ -13,33 +13,45 @@ import com.twitter.util.{Future => TwitterFuture}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Future => ScalaFuture}
-
+import scala.concurrent.duration._
 
 
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse
+//import org.elasticsearch.action.update.UpdateResponse
 import com.sksamuel.elastic4s.indexes.RichIndexResponse
 import com.sksamuel.elastic4s.bulk.RichBulkResponse
+import com.sksamuel.elastic4s.update.RichUpdateResponse
+
 
 
 class Persistency {
   val logger=org.slf4j.LoggerFactory.getLogger(getClass)
   import scala.concurrent.ExecutionContext.Implicits.global
   
-  val indexName=s"dummy-global"
+  val now=System.currentTimeMillis()
+  val sdf=new java.text.SimpleDateFormat("YYYY-MM-dd-H-m-s")
   
-  def _deleteIndex(client:ElasticClient):ScalaFuture[DeleteIndexResponse] = {
-    logger.info(s"deleting index $indexName (we're in debug mode)")
-    client.execute{deleteIndex(indexName)}
+  val paramIndexName="dummy-global"
+  val seriesIndexName="dummy-series-"+sdf.format(now)
+  
+  def _deleteIndex(implicit client:ElasticClient):ScalaFuture[DeleteIndexResponse] = {
+    logger.info(s"deleting index $paramIndexName (we're in debug mode)")
+    client.execute{deleteIndex(paramIndexName)}
   }
   
-  def _createIndex(client:ElasticClient):ScalaFuture[CreateIndexResponse] = {
-    logger.info(s"creating index $indexName")
-    client.execute{
-      createIndex(indexName) mappings(
+  def _createIndex(implicit client:ElasticClient):ScalaFuture[List[CreateIndexResponse]] = {
+    logger.info(s"creating index $paramIndexName")
+    val paramIndex = client.execute{
+      createIndex(paramIndexName) mappings(
         mapping("params") as Seq(
             textField("message")
-            ),
+            )
+      )
+    }
+    logger.info(s"creating index $seriesIndexName")
+    val seriesIndex = client.execute{
+      createIndex(seriesIndexName) mappings(
         mapping("cells") as Seq(
             keywordField("name"),
             dateField("time"),
@@ -47,33 +59,44 @@ class Persistency {
         )
       )
     }
+    ScalaFuture.sequence(paramIndex::seriesIndex::Nil)
   }
   
-  def _populate(client:ElasticClient):ScalaFuture[List[RichBulkResponse]] = {
-    logger.info(s"populating $indexName")
+//  def _populate(implicit client:ElasticClient):ScalaFuture[List[RichBulkResponse]] = {
+//    val params = _populateParams
+//    val series = _populateFakeSeries
+//    ScalaFuture.sequence(params::series)
+//  }
+  
+  def _populateParams(implicit client:ElasticClient):ScalaFuture[RichBulkResponse] = {
+    logger.info(s"populating $paramIndexName with params")
     val params = client.execute{
       bulk {
-        indexInto(indexName / "params").fields("message" -> "default - refreshed").id(1)
+        indexInto(paramIndexName / "params").fields("message" -> "default - refreshed").id(1)
       }
     }
-    val series = _populateFakeSeries(client)
-    ScalaFuture.sequence(params::series)
+    params
   }
-  def _populateFakeSeries(client:ElasticClient) = {
-    def now=System.currentTimeMillis()
-    val sampleData=1.to(7200).map(i=> now+i*1000 -> scala.math.random*10d)
-    val bulks = for {
-      grouped <- sampleData.grouped(500).toList
+  
+  def _populateFakeSeries(implicit client:ElasticClient) = {
+    case class Cell(name:String, time:Long, value:Double)
+    def mkcell(name:String, timeindex:Int) = Cell(name, now+timeindex*1000,scala.math.random*10d) 
+    def sampleData={
+      Stream.from(1).take(7200).map(ti => mkcell("x",ti)::mkcell("y",ti)::mkcell("z",ti)::Nil)
+    }
+    def bulks = for {
+      grouped <- sampleData.grouped(500)
     } yield {
       client.execute{
         bulk {
           for {
-            (time, value) <- grouped
+            cells <- grouped
+            cell <- cells
           } yield {
-          indexInto(indexName / "cells").fields(
-                "name"->"x",
-                "time"->time,
-                "value"->value
+          indexInto(seriesIndexName / "cells").fields(
+                "name"->cell.name,
+                "time"->cell.time,
+                "value"->cell.value
               )
           }
         }
@@ -82,33 +105,22 @@ class Persistency {
     bulks
   }
   
-  def initializeIfNeeded(client:ElasticClient) = {
+  def initializeIfNeeded(implicit client:ElasticClient) = {
     logger.info(s"Checking if some initialization is required at elasticsearch side")
-    val r = client
-      .execute{ indexExists(indexName) }
+    client
+      .execute{ indexExists(paramIndexName) }
       .flatMap{ idx => 
         if (idx.isExists) {
-          logger.info(s"$indexName index already exists")
-          for {
-           di <-_deleteIndex(client)
-           ci <-_createIndex(client)
-          } yield {
-              _populate(client)
-          }
+          logger.info(s"$paramIndexName index already exists")
+          _deleteIndex.flatMap(_ => _createIndex ).flatMap(_ => _populateParams) // TODO - no results check
         } else {
-          logger.info(s"$indexName index is missing")
-          for {
-            ci <-_createIndex(client)
-          } yield {
-            _populate(client)
-          }
+          logger.info(s"$paramIndexName index is missing")
+          _createIndex.flatMap(_ => _populateParams)   // TODO - no results check
         }
       }
-    r.await
-    logger.info("initializeIfNeeded RESULT = "+r.value)
   }
   
-  lazy val client = {
+  lazy val clientFuture:TwitterFuture[ElasticClient] = ScalaFuture {
     import scala.util.Properties._
     val elkUriConfigKey="ELK_URI"
     val defaultUri = "elasticsearch://localhost:9300"
@@ -118,19 +130,21 @@ class Persistency {
         .getOrElse(defaultUri)
     val elasticUri = ElasticsearchClientUri(uri)
     logger.info(s"Connecting to $elasticUri")
-    val client = ElasticClient.transport(elasticUri)
-    initializeIfNeeded(client)   
+    implicit val client = ElasticClient.transport(elasticUri)
     client
-  }
+  }.flatMap(cl => initializeIfNeeded(cl).map(_ => cl)).as[TwitterFuture[ElasticClient]] // to avoid await for init...
   
-  def getMessage:TwitterFuture[String] = {
+  def getMessage:TwitterFuture[String] = clientFuture.flatMap{ client=>
     client
-      .execute {get(1).from(indexName / "params")}
+      .execute {get(1).from(paramIndexName / "params")}
       .map(_.sourceField("message").toString)
       .as[TwitterFuture[String]]
   }
   
-  def setMessage(msg:String) = client.execute {
-    update(1).in(indexName / "params").docAsUpsert("message" -> msg)
+  def setMessage(msg:String) = clientFuture.flatMap{ client=> 
+      client
+        .execute {update(1).in(paramIndexName / "params").docAsUpsert("message" -> msg)}
+        .as[TwitterFuture[RichUpdateResponse]]
   }
+  
 }
